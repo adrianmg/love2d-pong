9d11486a60a8cd1618eab1ada60febe4a9f1eefe
Adrian Mato <adrianmg@github.com>
2020-04-01 20:53:59 -0700

    Printing text (sadpanda)

---
+++ main.lua
@@ -0,0 +1,37 @@
+push = require 'push'
+
+-- Configuration
+WINDOW_WIDTH, WINDOW_HEIGHT = love.window.getDesktopDimensions()
+WINDOW_WIDTH, WINDOW_HEIGHT = WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+ 
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  
+  font = love.graphics.newFont('res/font.ttf', 8)
+  love.graphics.setFont(font)
+  love.window.setTitle('Pong')
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true,
+    pixelperfect = true
+  })
+end
+
+function love.keypressed(key)
+  if (key == 'escape') then
+    print(key)
+    love.event.quit()
+  end
+end
+
+function love.draw()
+  push:apply('start')
+
+  love.graphics.printf('Hello Pong!', 0, (VIRTUAL_HEIGHT / 2 - 6), VIRTUAL_WIDTH, 'center')
+
+  push:apply('end')
+end
