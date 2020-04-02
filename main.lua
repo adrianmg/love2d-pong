a329bc3610ceb1ccaa94f89fff96b17b67f0fc56
Adrian Mato <adrianmg@github.com>
2020-04-02 00:07:31 -0700

    Paddles and ball added

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ VIRTUAL_HEIGHT = 248
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
   
-  font = love.graphics.newFont('res/font.ttf', 8)
+  font = love.graphics.newFont('res/font.ttf', 16)
   love.graphics.setFont(font)
   love.window.setTitle('Pong')
 
@@ -28,10 +28,35 @@ function love.keypressed(key)
   end
 end
 
+function drawLine ()
+  rx, ry = love.math.random( 0, love.graphics.getWidth() ), love.math.random( 0, love.graphics.getHeight() )
+  love.graphics.setColor( love.math.random( ), love.math.random( ), love.math.random( ) )
+  love.graphics.line( 0, 0, rx, ry )
+end
+
+function drawRectangle ()
+  rx, ry = love.math.random( 0, love.graphics.getWidth() ), love.math.random( 0, love.graphics.getHeight() )
+  love.graphics.setColor( love.math.random( ), love.math.random( ), love.math.random( ) )
+  love.graphics.rectangle('line',love.math.random( 0, love.graphics.getWidth() ), love.math.random( 0, love.graphics.getHeight() ), love.math.random( 0, love.graphics.getWidth() ), love.math.random( 0, love.graphics.getHeight() ))
+end
+
 function love.draw()
   push:apply('start')
 
-  love.graphics.printf('Hello Pong!', 0, (VIRTUAL_HEIGHT / 2 - 6), VIRTUAL_WIDTH, 'center')
+  love.graphics.clear(255, 0, 255)
+  love.graphics.printf('Pong!', 0, 16, VIRTUAL_WIDTH, 'center')
+
+  -- Render: Player
+  paddleInitialX = 16
+  paddleInitialY = 36
+  paddleSize = 40
+  love.graphics.rectangle('fill', paddleInitialX, paddleInitialY, 5, paddleSize)
+  -- Render: Enemy
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - paddleInitialX, VIRTUAL_HEIGHT - (paddleSize + paddleInitialY), 5, paddleSize)
+  
+  -- Render: Ball
+  ballSize = 4
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - (ballSize * 2), VIRTUAL_HEIGHT / 2 - (ballSize * 2), ballSize, ballSize)
 
   push:apply('end')
 end
